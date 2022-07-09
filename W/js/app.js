73844171106d7fdde1f4c4869accfd44e75f3618
Zach <zacbenincasa@gamil.com>
2022-07-09 10:07:28 -0600

    added loops

---
+++ W/js/app.js
@@ -0,0 +1,18 @@
+let counter = 0;
+while (counter <50){
+    console.log(counter)
+    counter=counter+1
+}
+
+let neg=0
+while(neg>-50){
+    console.log(neg)
+    neg=neg -1
+}
+
+
+let list=0
+while(list >100){
+    console.log(list)
+    list=list+2
+}
